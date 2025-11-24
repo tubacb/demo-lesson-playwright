1d65cf0feaa3c36445b9f8adedfe2eee606c4fe9
tuba bilgin <tubacobann@gmail.com>
2025-11-24 22:09:09 +0200

    beforeeach added

--- tests/ui/e2e-flow-simple.spec.ts
+++ tests/ui/e2e-flow-simple.spec.ts
@@ -3,26 +3,26 @@ import { LoginPage } from '../pages/login-page'
 import { faker } from '@faker-js/faker/locale/ar'
 import { PASSWORD, USERNAME } from '../../config/env-data'
 
-test('signIn button disabled when incorrect data inserted.', async ({ page }) => {
-  const authPage = new LoginPage(page)
+let authPage: LoginPage
+
+test.beforeEach(async ({ page }) => {
+  authPage = new LoginPage(page)
   await authPage.open()
+})
+test('signIn button disabled when incorrect data inserted.', async ({}) => {
   await authPage.usernameField.fill(faker.lorem.word(2))
   await authPage.passwordField.fill(faker.lorem.word(7))
   await expect(authPage.signInButton).toBeDisabled()
 })
 
-test('login with correct credentials and verify order creation page', async ({ page }) => {
-  const authPage = new LoginPage(page)
-  await authPage.open()
+test('login with correct credentials and verify order creation page', async ({}) => {
   const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
   await expect(orderCreationPage.mainPageLink).toBeVisible()
   await expect(orderCreationPage.userNameField).toBeVisible()
   await expect(orderCreationPage.orderCreatedButton).toBeVisible()
 })
 
-test('login with correct credentials and create order', async ({ page }) => {
-  const authPage = new LoginPage(page)
-  await authPage.open()
+test('login with correct credentials and create order', async ({}) => {
   const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
   await orderCreationPage.userNameField.fill(faker.lorem.word(2))
   await orderCreationPage.phoneField.fill(faker.lorem.word(6))
@@ -30,19 +30,13 @@ test('login with correct credentials and create order', async ({ page }) => {
   await expect(orderCreationPage.orderCreatedButton).toBeVisible()
 })
 
-test('login with correct credentials and logout', async ({ page }) => {
-  const authPage = new LoginPage(page)
-  await authPage.open()
+test('login with correct credentials and logout', async ({}) => {
   const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
   await orderCreationPage.logOutButton.click()
   await expect(authPage.signInButton).toBeVisible()
 })
 
-test('to show disabled order creation button and username, phone fields input errors and after login with correct credentials and fill the username and phone then clear them', async ({
-  page,
-}) => {
-  const authPage = new LoginPage(page)
-  await authPage.open()
+test('to show disabled order creation button and username, phone fields input errors and after login with correct credentials and fill the username and phone then clear them', async ({}) => {
   const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
   await orderCreationPage.userNameField.fill(faker.lorem.word(3))
   await orderCreationPage.userNameField.clear()
@@ -53,22 +47,14 @@ test('to show disabled order creation button and username, phone fields input er
   await expect(orderCreationPage.createOrderButton).toBeDisabled()
 })
 
-test('to show phone input error and disabled order creation button after login with correct credentials and fill phone less than 6 characters', async ({
-  page,
-}) => {
-  const authPage = new LoginPage(page)
-  await authPage.open()
+test('to show phone input error and disabled order creation button after login with correct credentials and fill phone less than 6 characters', async ({}) => {
   const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
   await orderCreationPage.phoneField.fill(faker.lorem.word(5))
   await expect(orderCreationPage.phoneInputError).toBeVisible()
   await expect(orderCreationPage.createOrderButton).toBeDisabled()
 })
 
-test('to create order after entering correct credentials for login and order pages and to close the popup with OK button and to log out', async ({
-  page,
-}) => {
-  const authPage = new LoginPage(page)
-  await authPage.open()
+test('to create order after entering correct credentials for login and order pages and to close the popup with OK button and to log out', async ({}) => {
   const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
   await orderCreationPage.userNameField.fill(faker.lorem.word(6))
   await orderCreationPage.phoneField.fill(faker.lorem.word(6))
